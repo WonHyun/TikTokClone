4731c881b91fe373c1a462e7c1fdea81234b9d4f
WonHyun Choi <coy4728@naver.com>
2024-05-22 22:00:45 +0900

    feat: implement auth button

---
+++ lib/features/authentication/widgets/auth_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/sizes.dart';
+
+class AuthButton extends StatelessWidget {
+  const AuthButton({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  final FaIcon icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 1.0,
+      child: Container(
+        padding: const EdgeInsets.all(Sizes.size14),
+        decoration: BoxDecoration(
+            border: Border.all(
+          color: Colors.grey.shade300,
+          width: Sizes.size1,
+        )),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(alignment: Alignment.centerLeft, child: icon),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
