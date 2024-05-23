e6920a3a88bfb94f7d5058ea3f6f0123acf96a34
WonHyun Choi <coy4728@naver.com>
2024-05-23 17:23:25 +0900

    feat: implement login form screen

---
+++ lib/features/authentication/login_form_screen.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:tictok_clone/constants/gaps.dart';
+import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/authentication/widgets/form_button.dart';
+
+class LoginFormScreen extends StatefulWidget {
+  const LoginFormScreen({super.key});
+
+  @override
+  State<LoginFormScreen> createState() => _LoginFormScreenState();
+}
+
+class _LoginFormScreenState extends State<LoginFormScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final Map<String, String> formData = {};
+  final bool _obscureText = true;
+
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
+  String? _isEmailValid(String? email) {
+    if (email == null || email.isEmpty) return "Enter your email.";
+    final regExp = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    if (!regExp.hasMatch(email)) {
+      return "Not valid email.";
+    }
+    return null;
+  }
+
+  String? _isPasswordValid(String? password) {
+    if (password == null || password.isEmpty) return "Enter your password.";
+    if (password.length <= 8) {
+      return "Your password must be at least 8 characters.";
+    }
+    return null;
+  }
+
+  // void _onClearTap() {}
+
+  // void _toggleObscureText() {
+  //   setState(() {
+  //     _obscureText = !_obscureText;
+  //   });
+  // }
+
+  void _onSubmitTap() {
+    if (_formKey.currentState != null) {
+      if (_formKey.currentState!.validate()) {
+        _formKey.currentState!.save();
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size36),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Gaps.v28,
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    return value == null
+                        ? "Email is empty"
+                        : _isEmailValid(value);
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['email'] = newValue;
+                    }
+                  },
+                ),
+                Gaps.v16,
+                TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).primaryColor,
+                      ),
+                    ),
+                  ),
+                  validator: (value) {
+                    return _isPasswordValid(value);
+                  },
+                  onSaved: (newValue) {
+                    if (newValue != null) {
+                      formData['password'] = newValue;
+                    }
+                  },
+                ),
+                Gaps.v28,
+                FormButton(
+                  text: "Next",
+                  onTap: _onSubmitTap,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
