45ac5a6840717b1cdefefd236a089604daf9eee4
WonHyun Choi <coy4728@naver.com>
2024-05-29 21:56:34 +0900

    feat: implement main navigation bar

--- lib/features/main_navigation/main_navigaion_screen.dart
+++ lib/features/main_navigation/main_navigaion_screen.dart
@@ -1,10 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/gaps.dart';
+import 'package:tictok_clone/constants/sizes.dart';
 
-class MainNavigaionScreen extends StatelessWidget {
+import 'widgets/nav_tab.dart';
+import 'widgets/post_video_button.dart';
+
+class MainNavigaionScreen extends StatefulWidget {
   const MainNavigaionScreen({super.key});
 
   @override
+  State<MainNavigaionScreen> createState() => _MainNavigaionScreenState();
+}
+
+class _MainNavigaionScreenState extends State<MainNavigaionScreen> {
+  int _selectedIndex = 0;
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: const Text("Record video"),
+          ),
+        ),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: Container(),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.magnifyingGlass,
+                selectedIcon: FontAwesomeIcons.compass,
+                onTap: () => _onTap(1),
+              ),
+              Gaps.h24,
+              PostVideoButton(onTap: _onPostVideoButtonTap),
+              Gaps.h24,
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/main_navigation/widgets/post_video_button.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/sizes.dart';
+
+class PostVideoButton extends StatefulWidget {
+  const PostVideoButton({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  State<PostVideoButton> createState() => _PostVideoButtonState();
+}
+
+class _PostVideoButtonState extends State<PostVideoButton> {
+  bool _isPressed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (details) => setState(() => _isPressed = true),
+      onTapUp: (details) => setState(() {
+        widget.onTap();
+        _isPressed = false;
+      }),
+      onTapCancel: () => setState(() => _isPressed = false),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            right: 20,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              height: _isPressed ? 40 : 30,
+              width: _isPressed ? 30 : 25,
+              padding: const EdgeInsets.symmetric(horizontal: Sizes.size8),
+              decoration: BoxDecoration(
+                color: const Color(0xFF61D4F0),
+                borderRadius: BorderRadius.circular(Sizes.size8),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 20,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              height: _isPressed ? 40 : 30,
+              width: _isPressed ? 30 : 25,
+              padding: const EdgeInsets.symmetric(horizontal: Sizes.size8),
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(Sizes.size8),
+              ),
+            ),
+          ),
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            height: _isPressed ? 40 : 30,
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size12),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(Sizes.size6),
+            ),
+            child: const Center(
+              child: FaIcon(
+                FontAwesomeIcons.plus,
+                size: 18,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
